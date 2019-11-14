0e15fa685246e661e765366714f5a0d78c5f084e
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-14 13:08:37 +0900

    Test Data and workflow set

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ git clone --depth 1 https://github.com/ynkdir/vim-vimlparser ${VIMLPARSER_PATH}
 #     - "%f:%l:%c:%m"
 
 sh "${VIMLINT_PATH}/bin/vimlint.sh" -l "${VIMLINT_PATH}" -p "${VIMLPARSER_PATH}"          \
-  -e EVL102.l:_=1 -c func_abort=1 autoload                                                \
+  -e EVL102.l:_=1 -c func_abort=1 testdata                                                \
    | reviewdog -efm="%f:%l:%c:%trror: %m" -efm="%f:%l:%c:%tarning: %m" -efm="%f:%l:%c:%m" \
                -name="vimlint" -reporter="${INPUT_REPORTER:-'github-pr-check'}"           \
                -level="${INPUT_LEVEL}"
