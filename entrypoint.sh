eae6ec96e497b47763422d0b8e88778f3d65542f
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-11-15 10:56:12 +0900

    Fix target param default value selection

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ git clone --depth 1 https://github.com/ynkdir/vim-vimlparser ${VIMLPARSER_PATH}
 #     - "%f:%l:%c:%m"
 
 sh "${VIMLINT_PATH}/bin/vimlint.sh" -l "${VIMLINT_PATH}" -p "${VIMLPARSER_PATH}"          \
-  -e EVL102.l:_=1 -c func_abort=1 ${INPUT_TARGET:-'autoload'}                             \
+  -e EVL102.l:_=1 -c func_abort=1 ${INPUT_TARGET:-autoload}                               \
    | reviewdog -efm="%f:%l:%c:%trror: %m" -efm="%f:%l:%c:%tarning: %m" -efm="%f:%l:%c:%m" \
                -name="vimlint" -reporter="${INPUT_REPORTER:-'github-pr-check'}"           \
                -level="${INPUT_LEVEL}"
